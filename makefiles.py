1c3a210f1468cf0ffae4725545369580a9503419
Karl Hobley <karlhobley10@gmail.com>
2015-05-28 21:47:53 +0100

    Fix Windows file paths

--- makefiles.py
+++ makefiles.py
@@ -5,7 +5,7 @@ layout=open("layout.html","r")
 
 directory=os.listdir("content")
 for file in directory:
-	infile=open("content\\"+file,"r")
+	infile=open("content/"+file,"r")
 	title=infile.readline().rstrip()
 	description=infile.readline().rstrip()
 	keywords=infile.readline().rstrip()
@@ -13,7 +13,7 @@ for file in directory:
 	for contentline in contentlines:
 		contentline=string.rstrip(string.lstrip(contentline))
 	content=''.join(contentlines)
-	outfile=open("output\\"+file,"w")
+	outfile=open("output/"+file,"w")
 	for line in iter(layout):
 		outfile.write(line.replace("%CONTENT%",content).replace("%KEYWORDS%",keywords).replace("%DESCRIPTION%",description).replace("%TITLE%",title).lstrip().rstrip())
 	layout.seek(0)
